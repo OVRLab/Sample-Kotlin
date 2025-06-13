76e1b00dc0181956ef0713aa826ba744b44d61c1
sedra155 <sedrasaade0@gmail.com>
2025-06-13 22:57:10 +0300

    Update AWS region to eu-central-1 (Frankfurt) as requested

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -2,7 +2,7 @@ variable "project_name" {
   default = "sample-kotlin"
 }
 variable "aws_region" {
-  default = "us-east-1"
+  default = "eu-central-1"
 }
 variable "container_port" {
   default = 8080
